8da087c4c5a06ffbf84f1fe23fc2f5f09e1adb09
Logan Waite <lwaite2010@gmail.com>
2020-04-06 08:10:32 -0400

    Discover Screen UI

--- features/discover/DiscoverScreen.js
+++ features/discover/DiscoverScreen.js
@@ -1,10 +1,48 @@
 import * as React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { BookTileWithButton } from '../../components/BookTile'
+import { Search } from '../../components/Search'
+import FAIcon from '../../components/FAIcon'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+import { Spacing } from '../../ui'
+
+
+function EmptyResultList () {
+  const searched = false
+  const text = searched ? "Sorry, we didn't find anything for your search." : "Start searching for something new!"
+  return (
+    <View style={styles.empty}>
+      <FAIcon icon="books" color={styles.emptyContent.color} size={50}></FAIcon>
+      <Text style={styles.emptyContent}>{text}</Text>
+    </View>
+  )
+}
 
 export default function DiscoverScreen () {
   return (
     <View>
-      <Text>Discover Screen</Text>
+      <Search></Search>
+      <EmptyResultList></EmptyResultList>
+      {/* <BookTileWithButton author="Jim Jones" title="book by jim jones"></BookTileWithButton>
+      <BookTileWithButton author="Jim Jones" title="book by jim jones"></BookTileWithButton>
+      <BookTileWithButton author="Jim Jones" title="book by jim jones"></BookTileWithButton>
+      <BookTileWithButton author="Jim Jones" title="book by jim jones"></BookTileWithButton>
+      <BookTileWithButton author="Jim Jones" title="book by jim jones"></BookTileWithButton> */}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  empty: {
+    // flex: 1,
+    height: '75%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emptyContent: {
+    color: '#aaa',
+    fontSize: 20,
+    margin: Spacing.medium,
+    textAlign: 'center'
+  }
+})
